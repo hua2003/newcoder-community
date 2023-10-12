ff06aa676a18f37e3ed7c2f13958cee907509a85
yynx <13013494+yynx@user.noreply.gitee.com>
2023-10-12 12:17:46 +0800

    实现发送邮件功能，实现MailUtil工具类

---
+++ src/test/java/com/newcoder/community/MailTests.java
@@ -0,0 +1,30 @@
+package com.newcoder.community;
+
+import com.newcoder.community.utils.MailUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@SpringBootTest
+public class MailTests {
+    @Autowired
+    private MailUtil mailUtil;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    void testSendMail() {
+        mailUtil.sendMail("2182523056@qq.com", "TEST", "hello world");
+    }
+
+    @Test
+    void testSendHtmlMail() {
+        Context context = new Context();
+        context.setVariable("username", "递归加训");
+        String content = templateEngine.process("/mail/demo", context);
+        mailUtil.sendMail("2182523056@qq.com", "TESTHTML", content);
+    }
+}
