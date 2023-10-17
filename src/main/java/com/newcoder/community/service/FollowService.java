d8f3c150d1454be836b9853705b9f2b4738735ab
yynx <1126964021@qq.com>
2023-10-18 00:26:18 +0800

    关注列表和粉丝列表

--- src/main/java/com/newcoder/community/service/FollowService.java
+++ src/main/java/com/newcoder/community/service/FollowService.java
@@ -1,9 +1,14 @@
 package com.newcoder.community.service;
 
+import java.util.List;
+import java.util.Map;
+
 public interface FollowService {
     void follow(int userId, int entityType, int entityId);
     void unfollow(int userId, int entityType, int entityId);
     long findFolloweeCount(int userId, int entityType);
     long findFollowerCount(int entityType, int entityId);
     boolean findFollowStatus(int userId, int entityType, int entityId);
+    List<Map<String, Object>> findFollowees(int userId, int offset, int limit);
+    List<Map<String, Object>> findFollowers(int userId, int offset, int limit);
 }
--- src/main/java/com/newcoder/community/service/impl/FollowServiceImpl.java
+++ src/main/java/com/newcoder/community/service/impl/FollowServiceImpl.java
@@ -1,6 +1,8 @@
 package com.newcoder.community.service.impl;
 
 import com.newcoder.community.service.FollowService;
+import com.newcoder.community.service.UserService;
+import com.newcoder.community.utils.CommunityConstant;
 import com.newcoder.community.utils.RedisKeyUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -9,10 +11,15 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SessionCallback;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 @Service
-public class FollowServiceImpl implements FollowService {
+public class FollowServiceImpl implements FollowService, CommunityConstant {
     @Autowired
     private RedisTemplate redisTemplate;
+
+    @Autowired
+    private UserService userService;
 
     @Override
     public void follow(int userId, int entityType, int entityId) {
@@ -60,5 +67,41 @@ public class FollowServiceImpl implements FollowService {
     public boolean findFollowStatus(int userId, int entityType, int entityId) {
         String followeeKey = RedisKeyUtil.getFolloweeKey(userId, entityType);
         return redisTemplate.opsForZSet().score(followeeKey, entityId) != null;
+    }
+
+    @Override
+    public List<Map<String, Object>> findFollowees(int userId, int offset, int limit) {
+        String followeeKey = RedisKeyUtil.getFolloweeKey(userId, ENTITY_TYPE_USER);
+        Set<Integer> set = redisTemplate.opsForZSet().reverseRange(followeeKey, offset, offset + limit - 1);
+
+        List<Map<String, Object>> list = new ArrayList<>(set.size());
+        if (set != null) {
+            for (Integer id : set) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("user", userService.findById(id));
+                Double score = redisTemplate.opsForZSet().score(followeeKey, id);
+                map.put("followTime", new Date(score.longValue()));
+                list.add(map);
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public List<Map<String, Object>> findFollowers(int userId, int offset, int limit) {
+        String followerKey = RedisKeyUtil.getFollowerKey(ENTITY_TYPE_USER, userId);
+        Set<Integer> set = redisTemplate.opsForZSet().reverseRange(followerKey, offset, offset + limit - 1);
+
+        List<Map<String, Object>> list = new ArrayList<>(set.size());
+        if (set != null) {
+            for (Integer id : set) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("user", userService.findById(id));
+                Double score = redisTemplate.opsForZSet().score(followerKey, id);
+                map.put("followTime", new Date(score.longValue()));
+                list.add(map);
+            }
+        }
+        return list;
     }
 }
